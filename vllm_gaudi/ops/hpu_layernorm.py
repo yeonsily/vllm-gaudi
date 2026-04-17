88658e00f2fddc44e82f95ad6c776f9acff2cbfc
slokesha <slokeshappa@habana.ai>
2026-04-17 21:24:14 +0000

    Added HPUGemmaRMSNorm
    Signed-off-by: slokesha <slokeshappa@habana.ai>

--- vllm_gaudi/ops/hpu_layernorm.py
+++ vllm_gaudi/ops/hpu_layernorm.py
@@ -1,7 +1,7 @@
 from typing import Optional, Union
 import torch
 from vllm.model_executor.layers.layernorm import \
-    RMSNorm
+    RMSNorm, GemmaRMSNorm
 
 
 @RMSNorm.register_oot
@@ -22,4 +22,26 @@ class HPURMSNorm(RMSNorm):
             return x.view(orig_shape), residual
 
         x = HPUFusedRMSNorm.apply(x, self.weight, self.variance_epsilon)
+        return x
+
+@GemmaRMSNorm.register_oot
+class HPUGemmaRMSNorm(GemmaRMSNorm):
+
+    def forward_oot(
+        self,
+        x: torch.Tensor,
+        residual: Optional[torch.Tensor] = None,
+    ) -> Union[torch.Tensor, tuple[torch.Tensor, torch.Tensor]]:
+        from vllm_gaudi.extension.kernels import rms_norm
+        HPUFusedRMSNorm = rms_norm()
+        # GemmaRMSNorm uses (1 + w) instead of w
+        gemma_weight = self.weight + 1.0
+        if residual is not None:
+            orig_shape = x.shape
+            residual = residual + x.view(residual.shape)
+            # Note: HPUFusedRMSNorm requires 3D tensors as inputs
+            x = HPUFusedRMSNorm.apply(residual, gemma_weight, self.variance_epsilon)
+            return x.view(orig_shape), residual
+
+        x = HPUFusedRMSNorm.apply(x, gemma_weight, self.variance_epsilon)
         return x
